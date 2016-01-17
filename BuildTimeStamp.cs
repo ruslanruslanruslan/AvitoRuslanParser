44a8cf6a9a28767c4765c080678ec912f3cd3b15
Vitalii Koshura <delionkur-lestat@mail.ru>
2016-01-18 01:52:09 +0200

    Another one

--- BuildTimeStamp.cs
+++ BuildTimeStamp.cs
@@ -1,10 +1,10 @@
-﻿namespace AvitoRuslanParser 
+namespace AvitoRuslanParser 
 { 
   class BuildTimeStamp 
   { 
     public static string TimeStamp() 
     { 
-      return "2016.01.17.23.46.09"; 
+      return "2016.01.17.23.49.19"; 
     } 
   } 
 } 
